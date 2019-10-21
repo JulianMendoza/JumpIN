dc6ff55c4ab94638aebf5db8d345dff3ab1cc69c
Giuseppe <Giuseppe@192.168.0.11>
2019-10-21 16:50:13 -0400

    LevelGenerator

--- src/jumpin/controller/GameController.java
+++ src/jumpin/controller/GameController.java
@@ -5,12 +5,9 @@ import jumpin.model.GameModel;
 import jumpin.view.GameView;
 
 public class GameController {
-<<<<<<< HEAD
-=======
 
 	private GameModel model;
 	private GameView view;
->>>>>>> branch 'gui' of https://github.com/JulianMendoza/JumpIN.git
 
 	public GameController(GameModel model, GameView view) {
 
--- src/jumpin/model/util/LevelGenerator.java
+++ src/jumpin/model/util/LevelGenerator.java
@@ -4,7 +4,7 @@ import jumpin.model.GameState;
 import jumpin.model.board.Board;
 import jumpin.model.constants.FoxPart;
 import jumpin.model.constants.Orientation;
-import jumpin.model.constants.PieceConstants;
+import jumpin.model.constants.PieceID;
 import jumpin.model.constants.StateOfGame;
 import jumpin.model.piece.pieces.Fox;
 import jumpin.model.piece.pieces.Mushroom;
@@ -26,15 +26,15 @@ public class LevelGenerator {
 	}
 
 	public void createLevel1() {
-		Fox fox = new Fox(FoxPart.HEAD, Orientation.NORTH_SOUTH, PieceConstants.FOX_ID_1);
-		Fox fox2 = new Fox(FoxPart.TAIL, Orientation.NORTH_SOUTH, PieceConstants.FOX_ID_1);
-		Rabbit rabbit = new Rabbit(PieceConstants.RABBIT_ID_1);
+		Fox fox = new Fox(FoxPart.HEAD, Orientation.NORTH_SOUTH, PieceID.FOX_ID_1);
+		Fox fox2 = new Fox(FoxPart.TAIL, Orientation.NORTH_SOUTH, PieceID.FOX_ID_1);
+		Rabbit rabbit = new Rabbit(PieceID.RABBIT_ID_1);
 		Mushroom mushroom = new Mushroom();
 		Mushroom mushroom2 = new Mushroom();
-		Fox foxb = new Fox(FoxPart.HEAD, Orientation.EAST_WEST, PieceConstants.FOX_ID_2);
-		Fox foxb2 = new Fox(FoxPart.TAIL, Orientation.EAST_WEST, PieceConstants.FOX_ID_2);
-		Rabbit rabbit2 = new Rabbit(PieceConstants.RABBIT_ID_2);
-		Rabbit rabbit3 = new Rabbit(PieceConstants.RABBIT_ID_3);
+		Fox foxb = new Fox(FoxPart.HEAD, Orientation.EAST_WEST, PieceID.FOX_ID_2);
+		Fox foxb2 = new Fox(FoxPart.TAIL, Orientation.EAST_WEST, PieceID.FOX_ID_2);
+		Rabbit rabbit2 = new Rabbit(PieceID.RABBIT_ID_2);
+		Rabbit rabbit3 = new Rabbit(PieceID.RABBIT_ID_3);
 		board.assignPiece(new Position(1, 0), fox2);
 		board.assignPiece(new Position(1, 1), fox);
 		board.assignPiece(new Position(3, 0), rabbit);

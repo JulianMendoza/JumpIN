d0774c6efe15a6170ccea0672353e43403bf0836
giuseppepapalia <giuseppepapalia@cb5107-07.labs.sce.carleton.ca>
2019-11-14 22:52:42 -0500

    fixed conflict

--- src/play/GUI.java
+++ src/play/GUI.java
@@ -9,12 +9,9 @@ public class GUI {
 
 	public static void main(String args[]) {
 		GameModel model = new GameModel();
-<<<<<<< HEAD
 		model.getGenerator().createLevel1();
 		//model.getBoard().stuff();
-=======
 		model.getGenerator().createLevelTest();
->>>>>>> branch 'master' of https://github.com/JulianMendoza/JumpIN.git
 		GameView view = new GameView(model);
 		GameController controller = new GameController(model, view);
 		controller.launch();

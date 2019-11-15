fc3590fadc6ea53c0e7d6921ef9d261a59068581
Cameron Davis <cameron_d14@hotmail.com>
2019-11-14 19:57:49 -0500

    MainMenu GUI

--- src/jumpin/view/menu/MainMenu.java
+++ src/jumpin/view/menu/MainMenu.java
@@ -7,12 +7,21 @@ import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import jumpin.model.GameModel;
 import jumpin.view.constants.ComponentSize;
+import javax.swing.JToggleButton;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JRadioButton;
 
 /**
  * 
@@ -21,30 +30,68 @@ import jumpin.view.constants.ComponentSize;
  */
 public class MainMenu extends JPanel {
 
-	private JButton undoButton, redoButton;
-	private JTextField gameStateField;
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -436968148338186761L;
 	private GameModel model;
+	private JButton undoButton, redoButton;
+	private JLabel gameStateLabel;
 	
 	public MainMenu(GameModel model) {
 		this.model = model;
 		
-		undoButton = new JButton("UNDO");
-		redoButton = new JButton("REDO");
+		undoButton = new JButton("\u2190 UNDO");
+		redoButton = new JButton("REDO \u2192");
 		addListeners();
 		undoButton.setEnabled(false);
 		redoButton.setEnabled(false);
-		gameStateField = new JTextField();
+		
+		gameStateLabel = new JLabel();
+		gameStateLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		setStateLabelText();
+		gameStateLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		
 		setBounds(0, 0, ComponentSize.MENU_WIDTH, ComponentSize.MENU_HEIGHT);
 		setMaximumSize(getSize());
 		setBackground(new Color(2, 145, 55));
-		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-		add(undoButton, BorderLayout.WEST);
-		add(redoButton, BorderLayout.WEST);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Toggle");
+		rdbtnNewRadioButton.setBackground(Color.GREEN);
+		
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addGap(247)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(gameStateLabel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addComponent(undoButton)
+							.addGap(18)
+							.addComponent(rdbtnNewRadioButton)
+							.addGap(18)
+							.addComponent(redoButton, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(260, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(19)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(redoButton, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+						.addComponent(rdbtnNewRadioButton)
+						.addComponent(undoButton, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(gameStateLabel, GroupLayout.PREFERRED_SIZE, 51, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(22, Short.MAX_VALUE))
+		);
+		setLayout(groupLayout);
+	}
+	
+	public void setStateLabelText() {
+		gameStateLabel.setText("<html># of Rabbits to win: " + model.getGameState().getNumToWin() + 
+				"<br>Current state of the game: " + model.getGameState().getState().toString() +  "</html>");
 	}
 
 	public void setRedo(boolean enabled) {
@@ -78,6 +125,7 @@ public class MainMenu extends JPanel {
 			} else if(e.getSource().equals(redoButton)) {
 				model.getBoard().redoMove();
 			}
+			setStateLabelText();
 		}
 	}
 }

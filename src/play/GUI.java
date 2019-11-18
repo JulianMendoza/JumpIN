ef9069ea6d4564b46d5a45ee02cd0f1aa5fda656
JOHN <johnwarde@cmail.carleton.ca>
2019-11-17 19:03:22 -0500

    added some things

--- src/play/GUI.java
+++ src/play/GUI.java
@@ -2,15 +2,13 @@ package play;
 
 import jumpin.controller.GameController;
 import jumpin.model.GameModel;
-import jumpin.model.util.Position;
 import jumpin.view.GameView;
 
 public class GUI {
 
 	public static void main(String args[]) throws CloneNotSupportedException {
 		GameModel model = new GameModel();
-		//model.getGenerator().createLevel1();
-		model.getGenerator().createLevelTest();
+		model.getGenerator().createLevelTest3();
 		GameView view = new GameView(model);
 		GameController controller = new GameController(model, view);
 		controller.launch();
